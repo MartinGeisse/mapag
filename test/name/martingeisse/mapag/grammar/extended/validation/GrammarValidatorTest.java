68b646ac389a6ace2bd506c42cd6404138349959
Martin Geisse <martingeisse@googlemail.com>
2017-10-16 15:59:23 +0200

    added junit-dataprovider and refactored testcase

--- test/name/martingeisse/mapag/grammar/extended/validation/GrammarValidatorTest.java
+++ test/name/martingeisse/mapag/grammar/extended/validation/GrammarValidatorTest.java
@@ -1,17 +1,22 @@
 package name.martingeisse.mapag.grammar.extended.validation;
 
 import com.google.common.collect.ImmutableList;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import name.martingeisse.mapag.grammar.Associativity;
 import name.martingeisse.mapag.grammar.extended.*;
 import name.martingeisse.mapag.grammar.extended.expression.SequenceExpression;
 import name.martingeisse.mapag.grammar.extended.expression.SymbolReference;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static name.martingeisse.mapag.grammar.extended.TestGrammarObjects.*;
 
 /**
  *
  */
+@RunWith(DataProviderRunner.class)
 public class GrammarValidatorTest {
 
 	@Test
@@ -81,31 +86,43 @@ public class GrammarValidatorTest {
 		new GrammarValidator(grammar).validate();
 	}
 
-	@Test(expected = IllegalStateException.class)
-	public void testUnknownSymbolReferenceInFirstProductionFirstPlace() {
-		ImmutableList<Production> productions = ImmutableList.of(
-			new Production("nt1", new SequenceExpression(new SymbolReference("unknown"), new SymbolReference("foo")))
-		);
-		Grammar grammar = new Grammar(PACKAGE_NAME, CLASS_NAME, TERMINALS, NONTERMINALS, PRECEDENCE_TABLE_EMPTY, START_NONTERMINAL_NAME, productions);
-		new GrammarValidator(grammar).validate();
+	@DataProvider
+	public static Object[][] getTestUnknownSymbolReferenceInSecondProductionData() {
+		return new Object[][] {
+			{
+				ImmutableList.of(
+					PRODUCTION_1,
+					new Production("nt2", new SequenceExpression(new SymbolReference("unknown"), new SymbolReference("bar"))),
+					new Production("nt1", new SequenceExpression(new SymbolReference("foo"), new SymbolReference("bar")))
+				)
+			},
+			{
+				ImmutableList.of(
+					PRODUCTION_1,
+					new Production("nt2", new SequenceExpression(new SymbolReference("foo"), new SymbolReference("unknown"))),
+					new Production("nt1", new SequenceExpression(new SymbolReference("foo"), new SymbolReference("bar")))
+				)
+			},
+			{
+				ImmutableList.of(
+					PRODUCTION_1,
+					new Production("nt1", new SequenceExpression(new SymbolReference("foo"), new SymbolReference("bar"))),
+					new Production("nt2", new SequenceExpression(new SymbolReference("unknown"), new SymbolReference("bar")))
+				)
+			},
+			{
+				ImmutableList.of(
+					PRODUCTION_1,
+					new Production("nt1", new SequenceExpression(new SymbolReference("foo"), new SymbolReference("bar"))),
+					new Production("nt2", new SequenceExpression(new SymbolReference("foo"), new SymbolReference("unknown")))
+				)
+			}
+		};
 	}
 
 	@Test(expected = IllegalStateException.class)
-	public void testUnknownSymbolReferenceInFirstProductionSecondPlace() {
-		ImmutableList<Production> productions = ImmutableList.of(
-			new Production("nt1", new SequenceExpression(new SymbolReference("foo"), new SymbolReference("unknown")))
-		);
-		Grammar grammar = new Grammar(PACKAGE_NAME, CLASS_NAME, TERMINALS, NONTERMINALS, PRECEDENCE_TABLE_EMPTY, START_NONTERMINAL_NAME, productions);
-		new GrammarValidator(grammar).validate();
-	}
-
-	// TODO use a data provider to test some more cases
-	@Test(expected = IllegalStateException.class)
-	public void testUnknownSymbolReferenceInSecondProduction() {
-		ImmutableList<Production> productions = ImmutableList.of(
-			new Production("nt1", new SequenceExpression(new SymbolReference("foo"), new SymbolReference("bar"))),
-			new Production("nt2", new SymbolReference("unknown"))
-		);
+	@UseDataProvider("getTestUnknownSymbolReferenceInSecondProductionData")
+	public void testUnknownSymbolReference(ImmutableList<Production> productions) {
 		Grammar grammar = new Grammar(PACKAGE_NAME, CLASS_NAME, TERMINALS, NONTERMINALS, PRECEDENCE_TABLE_EMPTY, START_NONTERMINAL_NAME, productions);
 		new GrammarValidator(grammar).validate();
 	}
