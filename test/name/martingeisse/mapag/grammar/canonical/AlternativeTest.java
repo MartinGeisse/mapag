9bc5e77fd4447c292cf0cdbfc59a14f4527129dd
Martin Geisse <martingeisse@googlemail.com>
2017-10-18 09:17:23 +0200

    testing extended grammar classes finished

--- test/name/martingeisse/mapag/grammar/canonical/AlternativeTest.java
+++ test/name/martingeisse/mapag/grammar/canonical/AlternativeTest.java
@@ -2,6 +2,7 @@ package name.martingeisse.mapag.grammar.canonical;
 
 import com.google.common.collect.ImmutableList;
 import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -80,64 +81,72 @@ public class AlternativeTest {
 	// methods
 	//
 
-	// TODO
+	@DataProvider
+	public static Object[][] getValidAlternatives() {
+		return new Object[][]{
+				{new Alternative(ImmutableList.of(), null)},
+				{new Alternative(ImmutableList.of(), "xyz")},
+				{new Alternative(EXPANSION, null)},
+				{new Alternative(EXPANSION, "xyz")},
+		};
+	}
 
-//	@DataProvider
-//	public static Object[][] getValidAlternatives() {
-//		return new Object[][]{
-//				{new Alternative(ImmutableList.of(), null)},
-//				{new Alternative(ImmutableList.of(), "xyz")},
-//				{new Alternative(EXPANSION, null)},
-//				{new Alternative(EXPANSION, "xyz")},
-//		};
-//	}
-//
-//
-//	@Test(expected = IllegalArgumentException.class)
-//	public void testVanishSymbolNullWithNullPrecedence() {
-//		new Alternative(EXPANSION, null).vanishSymbol(null);
-//	}
-//
-//	@Test(expected = IllegalArgumentException.class)
-//	public void testVanishSymbolNullWithNonNullPrecedence() {
-//		new Alternative(EXPANSION, "xyz").vanishSymbol(null);
-//	}
-//
-//	@Test(expected = IllegalArgumentException.class)
-//	public void testVanishSymbolNullInEmptyAlternativeWithNullPrecedence() {
-//		new Alternative(ImmutableList.of(), null).vanishSymbol(null);
-//	}
-//
-//	@Test(expected = IllegalArgumentException.class)
-//	public void testVanishSymbolNullInEmptyAlternativeWithNonNullPrecedence() {
-//		new Alternative(ImmutableList.of(), "xyz").vanishSymbol(null);
-//	}
-//
-//	@Test(expected = IllegalArgumentException.class)
-//	public void testVanishSymbolEmptyWithNullPrecedence() {
-//		new Alternative(EXPANSION).vanishSymbol("");
-//	}
-//
-//	@Test(expected = IllegalArgumentException.class)
-//	public void testVanishSymbolEmptyInEmptyAlternative() {
-//		new Alternative(ImmutableList.of()).vanishSymbol("");
-//	}
-//
-//	@Test
-//	public void testVanishSymbolInEmptyAlternative() {
-//		Assert.assertTrue(new Alternative(ImmutableList.of()).vanishSymbol("foo").getExpansion().isEmpty());
-//	}
-//
-//	@Test
-//	public void testVanishSymbol() {
-//		ImmutableList<String> expected = ImmutableList.of("bar", "baz", "abc");
-//		Assert.assertEquals(expected, new Alternative(EXPANSION).vanishSymbol("foo").getExpansion());
-//	}
-//
-//	@Test
-//	public void testVanishSymbolToEmptyAlternative() {
-//		ImmutableList<String> input = ImmutableList.of("bar", "bar");
-//		Assert.assertEquals(0, new Alternative(input).vanishSymbol("bar").getExpansion().size());
-//	}
+	@Test(expected = IllegalArgumentException.class)
+	@UseDataProvider("getValidAlternatives")
+	public void testVanishSymbolNull(Alternative alternative) {
+		alternative.vanishSymbol(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	@UseDataProvider("getValidAlternatives")
+	public void testVanishSymbolEmpty(Alternative alternative) {
+		alternative.vanishSymbol("");
+	}
+
+	@Test
+	public void testVanishSymbolInEmptyAlternative() {
+		Alternative alternative = new Alternative(ImmutableList.of(), "foo");
+		Assert.assertTrue(alternative.vanishSymbol("foo").getExpansion().isEmpty());
+		Assert.assertTrue(alternative.vanishSymbol("foo").getEffectivePrecedenceTerminal().equals("foo"));
+		Assert.assertTrue(alternative.vanishSymbol("bar").getExpansion().isEmpty());
+		Assert.assertTrue(alternative.vanishSymbol("bar").getEffectivePrecedenceTerminal().equals("foo"));
+	}
+
+	@Test
+	public void testVanishSymbol() {
+		{
+			Alternative alternative = new Alternative(EXPANSION, "foo");
+
+			Assert.assertEquals(ImmutableList.of("bar", "baz", "abc"), alternative.vanishSymbol("foo").getExpansion());
+			Assert.assertTrue(alternative.vanishSymbol("foo").getEffectivePrecedenceTerminal().equals("foo"));
+
+			Assert.assertEquals(ImmutableList.of("foo", "baz", "foo", "abc"), alternative.vanishSymbol("bar").getExpansion());
+			Assert.assertTrue(alternative.vanishSymbol("bar").getEffectivePrecedenceTerminal().equals("foo"));
+
+			Assert.assertEquals(ImmutableList.of(EXPANSION), alternative.vanishSymbol("xyz").getExpansion());
+			Assert.assertTrue(alternative.vanishSymbol("xyz").getEffectivePrecedenceTerminal().equals("foo"));
+		}
+		{
+			Alternative alternative = new Alternative(EXPANSION, "xyz");
+
+			Assert.assertEquals(ImmutableList.of("bar", "baz", "abc"), alternative.vanishSymbol("foo").getExpansion());
+			Assert.assertTrue(alternative.vanishSymbol("foo").getEffectivePrecedenceTerminal().equals("xyz"));
+
+			Assert.assertEquals(ImmutableList.of("foo", "baz", "foo", "abc"), alternative.vanishSymbol("bar").getExpansion());
+			Assert.assertTrue(alternative.vanishSymbol("bar").getEffectivePrecedenceTerminal().equals("xyz"));
+
+			Assert.assertEquals(ImmutableList.of(EXPANSION), alternative.vanishSymbol("xyz").getExpansion());
+			Assert.assertTrue(alternative.vanishSymbol("xyz").getEffectivePrecedenceTerminal().equals("xyz"));
+		}
+	}
+
+	@Test
+	public void testVanishSymbolToEmptyAlternative() {
+		ImmutableList<String> input = ImmutableList.of("bar", "bar");
+		Assert.assertEquals(0, new Alternative(input, "foo").vanishSymbol("bar").getExpansion().size());
+		Assert.assertEquals(0, new Alternative(input, "foo").vanishSymbol("bar").getEffectivePrecedenceTerminal().equals("foo"));
+		Assert.assertEquals(0, new Alternative(input, "bar").vanishSymbol("bar").getExpansion().size());
+		Assert.assertEquals(0, new Alternative(input, "bar").vanishSymbol("bar").getEffectivePrecedenceTerminal().equals("bar"));
+	}
 
 }
