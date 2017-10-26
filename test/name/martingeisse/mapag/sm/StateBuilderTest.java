45a2618a6e9a8a1ae460229afa1f07c47ce4db3b
Martin Geisse <martingeisse@googlemail.com>
2017-10-26 10:35:33 +0200

    testing

--- test/name/martingeisse/mapag/sm/StateBuilderTest.java
+++ test/name/martingeisse/mapag/sm/StateBuilderTest.java
@@ -30,26 +30,38 @@ public class StateBuilderTest {
 	private static final TerminalDefinition TERMINAL_3 = new TerminalDefinition("t3", 3, Associativity.RIGHT);
 
 	private static final NonterminalDefinition START_SYMBOL_1 = new NonterminalDefinition("startSymbol", ImmutableList.of(
-			new Alternative(ImmutableList.of("t1"), null)
+		new Alternative(ImmutableList.of("t1"), null)
 	));
 
 	private static final NonterminalDefinition START_SYMBOL_2 = new NonterminalDefinition("startSymbol", ImmutableList.of(
-			new Alternative(ImmutableList.of("t1"), null),
-			new Alternative(ImmutableList.of("t2"), null)
+		new Alternative(ImmutableList.of("t1"), null),
+		new Alternative(ImmutableList.of("t2"), null)
 	));
 
 	private static final NonterminalDefinition START_SYMBOL_3 = new NonterminalDefinition("startSymbol", ImmutableList.of(
-			new Alternative(ImmutableList.of("t3"), null),
-			new Alternative(ImmutableList.of("nt1"), null)
+		new Alternative(ImmutableList.of("t3"), null),
+		new Alternative(ImmutableList.of("nt1"), null)
 	));
 
 	private static final NonterminalDefinition NONTERMINAL_1 = new NonterminalDefinition("nt1", ImmutableList.of(
-			new Alternative(ImmutableList.of("t1"), null)
+		new Alternative(ImmutableList.of("t1"), null)
 	));
 
+	private static final NonterminalDefinition NONTERMINAL_2 = new NonterminalDefinition("nt2", ImmutableList.of(
+		new Alternative(ImmutableList.of("t2"), null)
+	));
 
+	private static final NonterminalDefinition NONTERMINAL_3 = new NonterminalDefinition("nt3", ImmutableList.of(
+		new Alternative(ImmutableList.of("t1", "nt1", "t2", "t2"), null)
+	));
 
+	private static final NonterminalDefinition NONTERMINAL_4 = new NonterminalDefinition("nt4", ImmutableList.of(
+		new Alternative(ImmutableList.of("t1", "nt1", "t2", "t3"), null)
+	));
 
+	private static final NonterminalDefinition NONTERMINAL_5 = new NonterminalDefinition("nt5", ImmutableList.of(
+		new Alternative(ImmutableList.of("t1", "nt1", "t3", "t3"), null)
+	));
 
 	//
 	// ----------------------------------------------------------------------------------------------------------------
@@ -71,47 +83,85 @@ public class StateBuilderTest {
 	public static Object[][] getTestData() {
 		return new Object[][]{
 
-				// just START_SYMBOL_1
-				{
-						ImmutableList.of(TERMINAL_1, TERMINAL_2, TERMINAL_3),
-						ImmutableList.of(START_SYMBOL_1),
-						ImmutableSet.of(new StateElement("startSymbol", START_SYMBOL_1.getAlternatives().get(0), 0, "%eof")),
-						ImmutableSet.of(),
-				},
+			// just START_SYMBOL_1
+			{
+				ImmutableList.of(TERMINAL_1, TERMINAL_2, TERMINAL_3),
+				ImmutableList.of(START_SYMBOL_1),
+				ImmutableSet.of(new StateElement("startSymbol", START_SYMBOL_1.getAlternatives().get(0), 0, "%eof")),
+				ImmutableSet.of(),
+			},
 
-				// START_SYMBOL_1, but starting from a dummy symbol that has this on the RHS
-				{
-						ImmutableList.of(TERMINAL_1, TERMINAL_2, TERMINAL_3),
-						ImmutableList.of(START_SYMBOL_1),
-						ImmutableSet.of(new StateElement("dummy", new Alternative(ImmutableList.of("startSymbol"), null), 0, "%eof")),
-						ImmutableSet.of(new StateElement("startSymbol", START_SYMBOL_1.getAlternatives().get(0), 0, "%eof")),
-				},
+			// START_SYMBOL_1, but starting from a dummy symbol that has this on the RHS
+			{
+				ImmutableList.of(TERMINAL_1, TERMINAL_2, TERMINAL_3),
+				ImmutableList.of(START_SYMBOL_1),
+				ImmutableSet.of(new StateElement("dummy", new Alternative(ImmutableList.of("startSymbol"), null), 0, "%eof")),
+				ImmutableSet.of(new StateElement("startSymbol", START_SYMBOL_1.getAlternatives().get(0), 0, "%eof")),
+			},
 
-				// START_SYMBOL_2, but starting from a dummy symbol that has this on the RHS
-				{
-						ImmutableList.of(TERMINAL_1, TERMINAL_2, TERMINAL_3),
-						ImmutableList.of(START_SYMBOL_2),
-						ImmutableSet.of(new StateElement("dummy", new Alternative(ImmutableList.of("startSymbol"), null), 0, "%eof")),
-						ImmutableSet.of(
-								new StateElement("startSymbol", START_SYMBOL_2.getAlternatives().get(0), 0, "%eof"),
-								new StateElement("startSymbol", START_SYMBOL_2.getAlternatives().get(1), 0, "%eof")
-						),
-				},
+			// START_SYMBOL_2, but starting from a dummy symbol that has this on the RHS
+			{
+				ImmutableList.of(TERMINAL_1, TERMINAL_2, TERMINAL_3),
+				ImmutableList.of(START_SYMBOL_2),
+				ImmutableSet.of(new StateElement("dummy", new Alternative(ImmutableList.of("startSymbol"), null), 0, "%eof")),
+				ImmutableSet.of(
+					new StateElement("startSymbol", START_SYMBOL_2.getAlternatives().get(0), 0, "%eof"),
+					new StateElement("startSymbol", START_SYMBOL_2.getAlternatives().get(1), 0, "%eof")
+				),
+			},
 
-				// START_SYMBOL_3 with a dummy symbol (double-indirection)
-				{
-						ImmutableList.of(TERMINAL_1, TERMINAL_2, TERMINAL_3),
-						ImmutableList.of(START_SYMBOL_3, NONTERMINAL_1),
-						ImmutableSet.of(new StateElement("dummy", new Alternative(ImmutableList.of("startSymbol"), null), 0, "%eof")),
-						ImmutableSet.of(
-								new StateElement("startSymbol", START_SYMBOL_3.getAlternatives().get(0), 0, "%eof"),
-								new StateElement("startSymbol", START_SYMBOL_3.getAlternatives().get(1), 0, "%eof"),
-								new StateElement("nt1", NONTERMINAL_1.getAlternatives().get(0), 0, "%eof")
-						),
-				},
+			// START_SYMBOL_3 with a dummy symbol (double-indirection)
+			{
+				ImmutableList.of(TERMINAL_1, TERMINAL_2, TERMINAL_3),
+				ImmutableList.of(START_SYMBOL_3, NONTERMINAL_1),
+				ImmutableSet.of(new StateElement("dummy", new Alternative(ImmutableList.of("startSymbol"), null), 0, "%eof")),
+				ImmutableSet.of(
+					new StateElement("startSymbol", START_SYMBOL_3.getAlternatives().get(0), 0, "%eof"),
+					new StateElement("startSymbol", START_SYMBOL_3.getAlternatives().get(1), 0, "%eof"),
+					new StateElement("nt1", NONTERMINAL_1.getAlternatives().get(0), 0, "%eof")
+				),
+			},
 
+			// next state does not include an element for which no nonterminal was reached
+			{
+				ImmutableList.of(TERMINAL_1, TERMINAL_2, TERMINAL_3),
+				ImmutableList.of(START_SYMBOL_3, NONTERMINAL_1, NONTERMINAL_2),
+				ImmutableSet.of(new StateElement("dummy", new Alternative(ImmutableList.of("startSymbol"), null), 0, "%eof")),
+				ImmutableSet.of(
+					new StateElement("startSymbol", START_SYMBOL_3.getAlternatives().get(0), 0, "%eof"),
+					new StateElement("startSymbol", START_SYMBOL_3.getAlternatives().get(1), 0, "%eof"),
+					new StateElement("nt1", NONTERMINAL_1.getAlternatives().get(0), 0, "%eof")
+				),
+			},
 
-				// TODO
+			// this test includes the same element twice, starting from two different previous elements
+			{
+				ImmutableList.of(TERMINAL_1, TERMINAL_2, TERMINAL_3),
+				ImmutableList.of(START_SYMBOL_3, NONTERMINAL_1, NONTERMINAL_3, NONTERMINAL_4),
+				ImmutableSet.of(
+					new StateElement("nt3", NONTERMINAL_3.getAlternatives().get(0), 1, "%eof"),
+					new StateElement("nt4", NONTERMINAL_4.getAlternatives().get(0), 1, "%eof")
+				),
+				ImmutableSet.of(
+					new StateElement("nt1", NONTERMINAL_1.getAlternatives().get(0), 0, "t2")
+				),
+			},
+
+			// similar, but this time it's not the same element twice, but two elements from different previous
+			// elements that are equal except for the lookahead terminal
+			{
+				ImmutableList.of(TERMINAL_1, TERMINAL_2, TERMINAL_3),
+				ImmutableList.of(START_SYMBOL_3, NONTERMINAL_1, NONTERMINAL_3, NONTERMINAL_5),
+				ImmutableSet.of(
+					new StateElement("nt3", NONTERMINAL_3.getAlternatives().get(0), 1, "%eof"),
+					new StateElement("nt5", NONTERMINAL_5.getAlternatives().get(0), 1, "%eof")
+				),
+				ImmutableSet.of(
+					new StateElement("nt1", NONTERMINAL_1.getAlternatives().get(0), 0, "t2"),
+					new StateElement("nt1", NONTERMINAL_1.getAlternatives().get(0), 0, "t3")
+				),
+			},
+
 		};
 	}
 
