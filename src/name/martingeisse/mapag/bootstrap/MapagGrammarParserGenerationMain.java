7a3e8e1c678bd163aba4c9d3949042738bd57ba5
Martin Geisse <martingeisse@googlemail.com>
2017-10-26 22:09:12 +0200

    bootstrapping grammar

--- src/name/martingeisse/mapag/bootstrap/MapagGrammarParserGenerationMain.java
+++ src/name/martingeisse/mapag/bootstrap/MapagGrammarParserGenerationMain.java
@@ -5,6 +5,7 @@ import name.martingeisse.mapag.codegen.MapagParserClassGenerator;
 import name.martingeisse.mapag.grammar.canonical.info.GrammarInfo;
 import name.martingeisse.mapag.grammar.canonicalization.GrammarCanonicalizer;
 import name.martingeisse.mapag.grammar.extended.*;
+import name.martingeisse.mapag.grammar.extended.expression.*;
 import name.martingeisse.mapag.sm.StateMachine;
 import name.martingeisse.mapag.sm.StateMachineBuilder;
 
@@ -20,7 +21,6 @@ public class MapagGrammarParserGenerationMain {
 
 		ImmutableList terminalDeclarations = ImmutableList.of(
 			new TerminalDeclaration("KW_PACKAGE"),
-			new TerminalDeclaration("KW_CLASS"),
 			new TerminalDeclaration("KW_CLASS"),
 			new TerminalDeclaration("KW_TERMINALS"),
 			new TerminalDeclaration("KW_NONTERMINALS"),
@@ -41,19 +41,17 @@ public class MapagGrammarParserGenerationMain {
 			new TerminalDeclaration("QUESTION_MARK"),
 			new TerminalDeclaration("ASTERISK"),
 			new TerminalDeclaration("PLUS"),
+			new TerminalDeclaration("BAR"),
 			new TerminalDeclaration("IDENTIFIER")
 		);
 
 		ImmutableList nonterminalDeclarations = ImmutableList.of(
 			new NonterminalDeclaration("grammar"),
-			new NonterminalDeclaration("symbolDefinitions"),
-			new NonterminalDeclaration("symbolDefinition"),
-			new NonterminalDeclaration("precedenceList"),
 			new NonterminalDeclaration("precedenceDeclaration"),
-			new NonterminalDeclaration("productionList"),
 			new NonterminalDeclaration("production"),
-			new NonterminalDeclaration("expression"),
-			new NonterminalDeclaration("hierarchicalIdentifier"),
+			new NonterminalDeclaration("alternative"),
+			new NonterminalDeclaration("toplevelExpression"),
+			new NonterminalDeclaration("nestedExpression"),
 			new NonterminalDeclaration("nonemptyIdentifierList")
 		);
 
@@ -63,7 +61,97 @@ public class MapagGrammarParserGenerationMain {
 		String startNonterminalName = "grammar";
 
 		ImmutableList<Production> productions = ImmutableList.of(
-
+			new Production("grammar", ImmutableList.of(
+				new Alternative(sequence(
+					symbol("KW_PACKAGE"),
+					new ZeroOrMoreExpression(new SequenceExpression(symbol("IDENTIFIER"), symbol("DOT"))),
+					symbol("IDENTIFIER"),
+					symbol("SEMICOLON"),
+					symbol("KW_CLASS"),
+					symbol("IDENTIFIER"),
+					symbol("SEMICOLON"),
+					symbol("KW_TERMINALS"),
+					symbol("OPENING_CURLY_BRACE"),
+					symbol("nonemptyIdentifierList"),
+					symbol("CLOSING_CURLY_BRACE"),
+					symbol("KW_NONTERMINALS"),
+					symbol("OPENING_CURLY_BRACE"),
+					symbol("nonemptyIdentifierList"),
+					symbol("CLOSING_CURLY_BRACE"),
+					new OptionalExpression(sequence(
+						symbol("KW_PRECEDENCE"),
+						symbol("OPENING_CURLY_BRACE"),
+						new ZeroOrMoreExpression(symbol("precedenceDeclaration")),
+						symbol("CLOSING_CURLY_BRACE")
+					)),
+					symbol("KW_START"),
+					symbol("IDENTIFIER"),
+					symbol("SEMICOLON"),
+					new OneOrMoreExpression(symbol("production"))
+				), null)
+			)),
+			new Production("precedenceDeclaration", ImmutableList.of(
+				new Alternative(sequence(
+					or(symbol("KW_LEFT"), symbol("KW_RIGHT"), symbol("KW_NONASSOC")),
+					symbol("nonemptyIdentifierList"),
+					symbol("SEMICOLON")
+				), null)
+			)),
+			new Production("production", ImmutableList.of(
+				new Alternative(sequence(
+					symbol("IDENTIFIER"),
+					symbol("COLON_COLON_EQUALS"),
+					symbol("alternative"),
+					new ZeroOrMoreExpression(sequence(symbol("BAR"), symbol("alternative"))),
+					symbol("SEMICOLON")
+				), null)
+			)),
+			new Production("alternative", ImmutableList.of(
+				new Alternative(sequence(
+					new OneOrMoreExpression(symbol("toplevelExpression")),
+					new OptionalExpression(sequence(symbol("KW_PRECEDENCE"), symbol("IDENTIFIER")))
+				), null)
+			)),
+			new Production("toplevelExpression", ImmutableList.of(
+				new Alternative(sequence(
+					symbol("IDENTIFIER"),
+					new OptionalExpression(sequence(symbol("COLON"), symbol("IDENTIFIER")))
+				), null),
+				new Alternative(sequence(symbol("toplevelExpression"), symbol("ASTERISK")), null),
+				new Alternative(sequence(symbol("toplevelExpression"), symbol("PLUS")), null),
+				new Alternative(sequence(symbol("toplevelExpression"), symbol("QUESTION_MARK")), null),
+				new Alternative(sequence(
+					symbol("OPENING_PARENTHESIS"),
+					new OneOrMoreExpression(symbol("nestedExpression")),
+					symbol("OPENING_PARENTHESIS"),
+					new OptionalExpression(sequence(symbol("COLON"), symbol("IDENTIFIER")))
+				), null)
+			)),
+			new Production("nestedExpression", ImmutableList.of(
+				new Alternative(sequence(
+					symbol("IDENTIFIER"),
+					new OptionalExpression(sequence(symbol("COLON"), symbol("IDENTIFIER")))
+				), null),
+				new Alternative(sequence(
+					symbol("nestedExpression"),
+					symbol("BAR"),
+					symbol("toplevelExpression")
+				), null),
+				new Alternative(sequence(symbol("nestedExpression"), symbol("ASTERISK")), null),
+				new Alternative(sequence(symbol("nestedExpression"), symbol("PLUS")), null),
+				new Alternative(sequence(symbol("nestedExpression"), symbol("QUESTION_MARK")), null),
+				new Alternative(sequence(
+					symbol("OPENING_PARENTHESIS"),
+					new OneOrMoreExpression(symbol("nestedExpression")),
+					symbol("OPENING_PARENTHESIS"),
+					new OptionalExpression(sequence(symbol("COLON"), symbol("IDENTIFIER")))
+				), null)
+			)),
+			new Production("nonemptyIdentifierList", ImmutableList.of(
+				new Alternative(
+					sequence(symbol("IDENTIFIER"), new ZeroOrMoreExpression(sequence(symbol("COMMA"), symbol("IDENTIFIER"))))
+					, null)
+			))
 		);
 
 		Grammar grammar = new Grammar(packageName, className, terminalDeclarations, nonterminalDeclarations, precedenceTable, startNonterminalName, productions);
@@ -74,66 +162,36 @@ public class MapagGrammarParserGenerationMain {
 		mapagParserClassGenerator.generate();
 	}
 
+	private static SymbolReference symbol(String name) {
+		return new SymbolReference(name);
+	}
+
+	private static Expression sequence(Expression... expressions) {
+		return sequence(0, expressions);
+	}
+
+	private static Expression sequence(int i, Expression... expressions) {
+		if (i == expressions.length) {
+			return new EmptyExpression();
+		} else if (i == expressions.length - 1) {
+			return expressions[i];
+		} else {
+			return new SequenceExpression(expressions[i], sequence(i + 1, expressions));
+		}
+	}
+
+	private static Expression or(Expression... expressions) {
+		return or(0, expressions);
+	}
+
+	private static Expression or(int i, Expression... expressions) {
+		if (i == expressions.length) {
+			return new EmptyExpression();
+		} else if (i == expressions.length - 1) {
+			return expressions[i];
+		} else {
+			return new OrExpression(expressions[i], or(i + 1, expressions));
+		}
+	}
+
 }
-
-/*
-
-%precedence {
-    %left BAR;
-    %nonassoc QUESTION_MARK, ASTERISK, PLUS;
-}
-
-%start grammar;
-
-
-//
-// grammar
-//
-
-grammar ::=
-	KW_PACKAGE (IDENTIFIER DOT)* IDENTIFIER SEMICOLON
-	KW_CLASS IDENTIFIER:className SEMICOLON
-	KW_TERMINALS OPENING_CURLY_BRACE nonemptyIdentifierList CLOSING_CURLY_BRACE
-	KW_NONTERMINALS OPENING_CURLY_BRACE nonemptyIdentifierList CLOSING_CURLY_BRACE
-	(KW_PRECEDENCE OPENING_CURLY_BRACE precedenceDeclaration* CLOSING_CURLY_BRACE)?
-	(KW_START IDENTIFIER:startSymbolName SEMICOLON)?
-	production+
-	;
-
-precedenceDeclaration ::=
-    (KW_LEFT | KW_RIGHT | KW_NONASSOC) nonemptyIdentifierList SEMICOLON
-    ;
-
-
-//
-// productions
-//
-
-production ::=
-	IDENTIFIER COLON_COLON_EQUALS expression SEMICOLON
-	| error SEMICOLON
-	;
-
-expression ::=
-    (
-    	IDENTIFIER (COLON IDENTIFIER)?
-	    | expression BAR expression
-	    | expression ASTERISK
-	    | expression PLUS
-	    | expression QUESTION_MARK
-	    | OPENING_PARENTHESIS expression CLOSING_PARENTHESIS (COLON IDENTIFIER)?
-	)+
-	;
-
-
-//
-// helpers
-//
-
-nonemptyIdentifierList ::=
-	IDENTIFIER
-	| nonemptyIdentifierList COMMA IDENTIFIER
-	;
-
-
- */
