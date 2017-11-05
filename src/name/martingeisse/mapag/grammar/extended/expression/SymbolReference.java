5394236e1479c0e7bb6267573828658e160f11aa
Martin Geisse <martingeisse@googlemail.com>
2017-11-05 12:20:29 +0100

    tests and fixes

--- src/name/martingeisse/mapag/grammar/extended/expression/SymbolReference.java
+++ src/name/martingeisse/mapag/grammar/extended/expression/SymbolReference.java
@@ -12,7 +12,7 @@ public final class SymbolReference extends Expression {
 
 	public SymbolReference(String symbolName) {
 		ParameterUtil.ensureNotNullOrEmpty(symbolName, "symbolName");
-		if (symbolName.startsWith("%") && !symbolName.equals(SpecialSymbols.ERROR_SYMBOL_NAME)) { // TODO test this
+		if (symbolName.startsWith("%") && !symbolName.equals(SpecialSymbols.ERROR_SYMBOL_NAME)) {
 			throw new IllegalArgumentException("invalid symbol reference: " + symbolName);
 		}
 		this.symbolName = symbolName;
