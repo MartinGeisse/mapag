3136aaea40afd39e91dd6aa545d161042a4375b2
Martin Geisse <martingeisse@googlemail.com>
2017-11-13 08:58:36 +0100

    PSI generation for repetition, this time it works

--- src/name/martingeisse/mapag/codegen/PsiClassesGenerator.java
+++ src/name/martingeisse/mapag/codegen/PsiClassesGenerator.java
@@ -34,20 +34,47 @@ public class PsiClassesGenerator {
 
 	public void generate() throws ConfigurationException {
 		for (NonterminalDefinition nonterminalDefinition : grammar.getNonterminalDefinitions().values()) {
-			if (nonterminalDefinition.getAlternatives().size() == 1) {
-				generateSingleAlternativeClass(nonterminalDefinition.getName(), nonterminalDefinition.getAlternatives().get(0));
-			} else {
-				generateMultiAlternativeBaseClass(nonterminalDefinition.getName());
-				List<Alternative> sortedAlternatives = new ArrayList<>(nonterminalDefinition.getAlternatives());
-				sortedAlternatives.sort(Comparators.alternativeComparator);
-				alternativeCounter = 0;
-				for (Alternative alternative : sortedAlternatives) {
-					generateMultiAlternativeCaseClass(nonterminalDefinition.getName(), alternative);
-					alternativeCounter++;
-				}
-			}
+			handleNonterminal(nonterminalDefinition);
 		}
 		generateFactoryClass();
+	}
+
+	private void handleNonterminal(NonterminalDefinition nonterminalDefinition) throws ConfigurationException {
+		switch (nonterminalDefinition.getAnnotation().getPsiStyle()) {
+
+			case NORMAL:
+			case OPTIONAL:
+				// TODO optionals
+				handleNormalStyledNonterminal(nonterminalDefinition);
+				break;
+
+			case ZERO_OR_MORE:
+				handleRepetitionStyledNonterminal(nonterminalDefinition, true);
+				break;
+
+			case ONE_OR_MORE:
+				handleRepetitionStyledNonterminal(nonterminalDefinition, false);
+				break;
+
+			default:
+				throw new RuntimeException("unknown PSI style: " + nonterminalDefinition.getAnnotation().getPsiStyle());
+
+		}
+	}
+
+	private void handleNormalStyledNonterminal(NonterminalDefinition nonterminalDefinition) throws ConfigurationException {
+		if (nonterminalDefinition.getAlternatives().size() == 1) {
+			generateSingleAlternativeClass(nonterminalDefinition.getName(), nonterminalDefinition.getAlternatives().get(0));
+		} else {
+			generateMultiAlternativeBaseClass(nonterminalDefinition.getName());
+			List<Alternative> sortedAlternatives = new ArrayList<>(nonterminalDefinition.getAlternatives());
+			sortedAlternatives.sort(Comparators.alternativeComparator);
+			alternativeCounter = 0;
+			for (Alternative alternative : sortedAlternatives) {
+				generateMultiAlternativeCaseClass(nonterminalDefinition.getName(), alternative);
+				alternativeCounter++;
+			}
+		}
 	}
 
 	private void generateSingleAlternativeClass(String nonterminalName, Alternative alternative) throws ConfigurationException {
@@ -78,12 +105,94 @@ public class PsiClassesGenerator {
 		classGenerator.generate();
 	}
 
+	private void handleRepetitionStyledNonterminal(NonterminalDefinition nonterminalDefinition, boolean zeroBased) throws ConfigurationException {
+
+		// verify the nonterminal's structure and determine its element symbol and Java type
+		String nonterminalName = nonterminalDefinition.getName();
+		if (nonterminalDefinition.getAlternatives().size() != 2) {
+			throw new RuntimeException("repetition-styled nonterminal " + nonterminalName + " has " +
+				nonterminalDefinition.getAlternatives().size() + " alternatives, expected 2");
+		}
+		Alternative baseCaseAlternative, repetitionCaseAlternative;
+		if (nonterminalDefinition.getAlternatives().get(0).getExpansion().size() == 2) {
+			repetitionCaseAlternative = nonterminalDefinition.getAlternatives().get(0);
+			baseCaseAlternative = nonterminalDefinition.getAlternatives().get(1);
+		} else if (nonterminalDefinition.getAlternatives().get(1).getExpansion().size() == 2) {
+			baseCaseAlternative = nonterminalDefinition.getAlternatives().get(0);
+			repetitionCaseAlternative = nonterminalDefinition.getAlternatives().get(1);
+		} else {
+			throw new RuntimeException("could not find alternative with expansion length 2 as repetition case for repetition-styled nonterminal " + nonterminalName);
+		}
+		if (baseCaseAlternative.getExpansion().size() != (zeroBased ? 0 : 1)) {
+			throw new RuntimeException("could not recognize base case for repetition-styled nonterminal " + nonterminalName);
+		}
+		if (!repetitionCaseAlternative.getExpansion().get(0).equals(nonterminalName)) {
+			throw new RuntimeException("could not find left-recursion for repetition-styled nonterminal " + nonterminalName);
+		}
+		String elementSymbol = repetitionCaseAlternative.getExpansion().get(1);
+		if (!zeroBased && !baseCaseAlternative.getExpansion().get(0).equals(elementSymbol)) {
+			throw new RuntimeException("base-case uses different element symbol than repetition case for nonterminal " + elementSymbol);
+		}
+		String elementType = psiTypeMapper.getEffectiveTypeForSymbol(elementSymbol);
+
+		// generate abstract class
+		String abstractClassName = psiTypeMapper.getEffectiveTypeForNonterminal(nonterminalName);
+		{
+			PsiClassGenerator classGenerator = new PsiClassGenerator();
+			classGenerator.className = abstractClassName;
+			classGenerator.superclass = "ASTWrapperPsiElement";
+			classGenerator.isAbstract = true;
+			classGenerator.alternative = null;
+			classGenerator.isRepetitionAbstract = true;
+			classGenerator.elementType = elementType;
+			classGenerator.isZeroBasedRepetition = zeroBased;
+			classGenerator.generate();
+		}
+
+		// generate base case class
+		{
+			// note: this fallback shouldn't be necessary since the alternatives of a repetition should always be named
+			String alternativeName = (baseCaseAlternative.getAnnotation().getAlternativeName() == null ? "baseCase" : baseCaseAlternative.getAnnotation().getAlternativeName());
+			PsiClassGenerator classGenerator = new PsiClassGenerator();
+			classGenerator.className = psiTypeMapper.toIdentifier(nonterminalName + '/' + alternativeName, true);
+			classGenerator.superclass = abstractClassName;
+			classGenerator.isAbstract = false;
+			classGenerator.alternative = baseCaseAlternative;
+			classGenerator.isRepetitionBaseCase = true;
+			classGenerator.elementType = elementType;
+			classGenerator.isZeroBasedRepetition = zeroBased;
+			classGenerator.generate();
+		}
+
+		// generate repetition case class
+		{
+			// note: this fallback shouldn't be necessary since the alternatives of a repetition should always be named
+			String alternativeName = (repetitionCaseAlternative.getAnnotation().getAlternativeName() == null ? "repetitionCase" : repetitionCaseAlternative.getAnnotation().getAlternativeName());
+			PsiClassGenerator classGenerator = new PsiClassGenerator();
+			classGenerator.className = psiTypeMapper.toIdentifier(nonterminalName + '/' + alternativeName, true);
+			classGenerator.superclass = abstractClassName;
+			classGenerator.isAbstract = false;
+			classGenerator.alternative = repetitionCaseAlternative;
+			classGenerator.isRepetitionNextCase = true;
+			classGenerator.elementType = elementType;
+			classGenerator.isZeroBasedRepetition = zeroBased;
+			classGenerator.generate();
+		}
+
+	}
+
 	private class PsiClassGenerator {
 
 		String className;
 		String superclass;
 		boolean isAbstract;
 		Alternative alternative;
+
+		boolean isRepetitionAbstract;
+		boolean isRepetitionBaseCase;
+		boolean isRepetitionNextCase;
+		String elementType;
+		boolean isZeroBasedRepetition;
 
 		void generate() throws ConfigurationException {
 
@@ -92,6 +201,11 @@ public class PsiClassesGenerator {
 			context.put("className", className);
 			context.put("superclass", superclass);
 			context.put("classModifiers", isAbstract ? "abstract" : "final");
+			context.put("isRepetitionAbstract", isRepetitionAbstract);
+			context.put("isRepetitionBaseCase", isRepetitionBaseCase);
+			context.put("isRepetitionNextCase", isRepetitionNextCase);
+			context.put("elementType", elementType);
+			context.put("isZeroBasedRepetition", isZeroBasedRepetition);
 
 			List<NodeGetter> nodeGetters = new ArrayList<>();
 			if (alternative != null && alternative.getAnnotation().getExpressionNames() != null) {
--- src/name/martingeisse/mapag/codegen/PsiTypeMapper.java
+++ src/name/martingeisse/mapag/codegen/PsiTypeMapper.java
@@ -1,9 +1,6 @@
 package name.martingeisse.mapag.codegen;
 
-import name.martingeisse.mapag.grammar.canonical.Alternative;
 import name.martingeisse.mapag.grammar.canonical.Grammar;
-import name.martingeisse.mapag.grammar.canonical.NonterminalAnnotation;
-import name.martingeisse.mapag.grammar.canonical.NonterminalDefinition;
 import name.martingeisse.mapag.grammar.canonical.info.GrammarInfo;
 
 /**
@@ -32,58 +29,9 @@ public class PsiTypeMapper {
 	}
 
 	public String getEffectiveTypeForNonterminal(String nonterminal) {
-		NonterminalDefinition nonterminalDefinition = grammar.getNonterminalDefinitions().get(nonterminal);
-		NonterminalAnnotation.PsiStyle psiStyle = nonterminalDefinition.getAnnotation().getPsiStyle();
-		switch (psiStyle) {
-
-			case NORMAL:
-				return getGeneratedTypeForNonterminal(nonterminal);
-
-			case OPTIONAL:
-				// TODO
-				return getGeneratedTypeForNonterminal(nonterminal);
-
-			case ZERO_OR_MORE:
-				return getCollectionTypeForNonterminal(nonterminalDefinition, true);
-
-			case ONE_OR_MORE:
-				return getCollectionTypeForNonterminal(nonterminalDefinition, false);
-
-			default:
-				return getGeneratedTypeForNonterminal(nonterminal);
-
-		}
+		return getGeneratedTypeForNonterminal(nonterminal);
 	}
 
-	private String getCollectionTypeForNonterminal(NonterminalDefinition nonterminalDefinition, boolean zeroAllowed) {
-		String nonterminalName = nonterminalDefinition.getName();
-		if (nonterminalDefinition.getAlternatives().size() != 2) {
-			throw new RuntimeException("collection-type nonterminal " + nonterminalName + " has " +
-				nonterminalDefinition.getAlternatives().size() + " alternatives, expected 2");
-		}
-		Alternative baseCaseAlternative, repetitionCaseAlternative;
-		if (nonterminalDefinition.getAlternatives().get(0).getExpansion().size() == 2) {
-			repetitionCaseAlternative = nonterminalDefinition.getAlternatives().get(0);
-			baseCaseAlternative = nonterminalDefinition.getAlternatives().get(1);
-		} else if (nonterminalDefinition.getAlternatives().get(1).getExpansion().size() == 2) {
-			baseCaseAlternative = nonterminalDefinition.getAlternatives().get(0);
-			repetitionCaseAlternative = nonterminalDefinition.getAlternatives().get(1);
-		} else {
-			throw new RuntimeException("could not find alternative with expansion length 2 as repetition case for repetition-type nonterminal " + nonterminalName);
-		}
-		if (baseCaseAlternative.getExpansion().size() != (zeroAllowed ? 0 : 1)) {
-			throw new RuntimeException("could not recognize base case for repetition-type nonterminal " + nonterminalName);
-		}
-		if (!repetitionCaseAlternative.getExpansion().get(0).equals(nonterminalName)) {
-			throw new RuntimeException("could not find left-recursion for repetition-type nonterminal " + nonterminalName);
-		}
-		String elementSymbol = repetitionCaseAlternative.getExpansion().get(1);
-		if (!zeroAllowed && !baseCaseAlternative.getExpansion().get(0).equals(elementSymbol)) {
-			throw new RuntimeException("base-case uses different element symbol than repetition case for nonterminal " + elementSymbol);
-		}
-		String elementType = getEffectiveTypeForSymbol(elementSymbol);
-		return "List<" + elementType + ">";
-	}
 
 	public String getGeneratedTypeForNonterminal(String nonterminal) {
 		return toIdentifier(nonterminal, true);
