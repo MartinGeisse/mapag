67e9f6799a364ae68785c641ddbc768692e119e5
Martin Geisse <martingeisse@googlemail.com>
2017-11-22 15:39:05 +0100

    more unit tests

---
+++ test/name/martingeisse/mapag/grammar/canonical/NonterminalDefinitionTest.java
@@ -0,0 +1,57 @@
+package name.martingeisse.mapag.grammar.canonical;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class NonterminalDefinitionTest {
+
+	private static final Alternative ALTERNATIVE_1 = new Alternative("a1", TestUtil.expansion("foo", "bar"), null);
+	private static final Alternative ALTERNATIVE_2 = new Alternative("a2", TestUtil.expansion("a", "b"), null);
+	private static final ImmutableList<Alternative> ALTERNATIVES = ImmutableList.of(ALTERNATIVE_1, ALTERNATIVE_2);
+
+	@Test
+	public void testConstructorGetter() {
+		NonterminalDefinition nonterminalDefinition = new NonterminalDefinition("nt", ALTERNATIVES, NonterminalDefinition.PsiStyle.NORMAL);
+		Assert.assertEquals("nt", nonterminalDefinition.getName());
+		Assert.assertEquals(ALTERNATIVES, nonterminalDefinition.getAlternatives());
+		Assert.assertEquals(NonterminalDefinition.PsiStyle.NORMAL, nonterminalDefinition.getPsiStyle());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testNullName() {
+		new NonterminalDefinition(null, ALTERNATIVES, NonterminalDefinition.PsiStyle.NORMAL);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testEmptyName() {
+		new NonterminalDefinition("", ALTERNATIVES, NonterminalDefinition.PsiStyle.NORMAL);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testNullAlternatives() {
+		new NonterminalDefinition("nt", null, NonterminalDefinition.PsiStyle.NORMAL);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testEmptyAlternatives() {
+		new NonterminalDefinition("nt", ImmutableList.of(), NonterminalDefinition.PsiStyle.NORMAL);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testPercentSignInNameNotAllowed() {
+		new NonterminalDefinition("%foo", ALTERNATIVES, NonterminalDefinition.PsiStyle.NORMAL);
+	}
+
+	@Test
+	public void testDifferentPsiStyle() {
+		NonterminalDefinition nonterminalDefinition = new NonterminalDefinition("nt", ALTERNATIVES, NonterminalDefinition.PsiStyle.OPTIONAL);
+		Assert.assertEquals("nt", nonterminalDefinition.getName());
+		Assert.assertEquals(ALTERNATIVES, nonterminalDefinition.getAlternatives());
+		Assert.assertEquals(NonterminalDefinition.PsiStyle.OPTIONAL, nonterminalDefinition.getPsiStyle());
+	}
+
+}
