02bb86118b3a809597bd438cfd64a4621d98d6fd
Martin Geisse <martingeisse@googlemail.com>
2017-11-23 08:23:17 +0100

    more unit tests

--- test/name/martingeisse/mapag/grammar/canonicalization/GrammarCanonicalizerTest.java
+++ test/name/martingeisse/mapag/grammar/canonicalization/GrammarCanonicalizerTest.java
@@ -3,6 +3,7 @@ package name.martingeisse.mapag.grammar.canonicalization;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import name.martingeisse.mapag.grammar.Associativity;
+import name.martingeisse.mapag.grammar.canonical.AlternativeConflictResolver;
 import name.martingeisse.mapag.grammar.extended.*;
 import name.martingeisse.mapag.grammar.extended.expression.SequenceExpression;
 import name.martingeisse.mapag.grammar.extended.expression.SymbolReference;
@@ -37,15 +38,15 @@ public class GrammarCanonicalizerTest {
 	);
 
 	private static final PrecedenceTable PRECEDENCE_TABLE = new PrecedenceTable(ImmutableList.of(
-		new PrecedenceTable.Entry(ImmutableSet.of("e"), Associativity.LEFT),
-		new PrecedenceTable.Entry(ImmutableSet.of("f", "a"), Associativity.RIGHT),
-		new PrecedenceTable.Entry(ImmutableSet.of("c"), Associativity.NONASSOC)
+		new PrecedenceTable.Entry(ImmutableList.of("e"), Associativity.LEFT),
+		new PrecedenceTable.Entry(ImmutableList.of("f", "a"), Associativity.RIGHT),
+		new PrecedenceTable.Entry(ImmutableList.of("c"), Associativity.NONASSOC)
 	));
 
-	private static final Alternative ALTERNATIVE_1 = new Alternative(null, new SymbolReference("a"), null);
-	private static final Alternative ALTERNATIVE_2 = new Alternative(null, new SymbolReference("p"), "f");
-	private static final Alternative ALTERNATIVE_3 = new Alternative(null, new SequenceExpression(new SymbolReference("f"), new SymbolReference("g")), null);
-	private static final Alternative ALTERNATIVE_4 = new Alternative(null, new SymbolReference("e"), null);
+	private static final Alternative ALTERNATIVE_1 = new Alternative(null, new SymbolReference("a"), null, null);
+	private static final Alternative ALTERNATIVE_2 = new Alternative(null, new SymbolReference("p"), "f", null);
+	private static final Alternative ALTERNATIVE_3 = new Alternative(null, new SequenceExpression(new SymbolReference("f"), new SymbolReference("g")), null, null);
+	private static final Alternative ALTERNATIVE_4 = new Alternative(null, new SymbolReference("e"), null, null);
 	private static final Production PRODUCTION_1 = new Production("p", ImmutableList.of(ALTERNATIVE_1, ALTERNATIVE_2));
 	private static final Production PRODUCTION_2 = new Production("q", ImmutableList.of(ALTERNATIVE_3));
 	private static final Production PRODUCTION_3 = new Production("start", ImmutableList.of(ALTERNATIVE_4));
@@ -105,25 +106,25 @@ public class GrammarCanonicalizerTest {
 
 		Assert.assertEquals("p", canonicalGrammar.getNonterminalDefinitions().get("p").getName());
 		Assert.assertEquals(2, canonicalGrammar.getNonterminalDefinitions().get("p").getAlternatives().size());
-		Assert.assertNull(canonicalGrammar.getNonterminalDefinitions().get("p").getAlternatives().get(0).getEffectivePrecedenceTerminal());
-		Assert.assertEquals(1, canonicalGrammar.getNonterminalDefinitions().get("p").getAlternatives().get(0).getExpansion().size());
-		Assert.assertEquals("a", canonicalGrammar.getNonterminalDefinitions().get("p").getAlternatives().get(0).getExpansion().get(0));
-		Assert.assertEquals("f", canonicalGrammar.getNonterminalDefinitions().get("p").getAlternatives().get(1).getEffectivePrecedenceTerminal());
-		Assert.assertEquals(1, canonicalGrammar.getNonterminalDefinitions().get("p").getAlternatives().get(1).getExpansion().size());
-		Assert.assertEquals("p", canonicalGrammar.getNonterminalDefinitions().get("p").getAlternatives().get(1).getExpansion().get(0));
+		Assert.assertNull(getEffectivePrecedenceTerminal(canonicalGrammar.getNonterminalDefinitions().get("p").getAlternatives().get(0)));
+		Assert.assertEquals(1, canonicalGrammar.getNonterminalDefinitions().get("p").getAlternatives().get(0).getExpansion().getElements().size());
+		Assert.assertEquals("a", canonicalGrammar.getNonterminalDefinitions().get("p").getAlternatives().get(0).getExpansion().getElements().get(0).getSymbol());
+		Assert.assertEquals("f", getEffectivePrecedenceTerminal(canonicalGrammar.getNonterminalDefinitions().get("p").getAlternatives().get(1)));
+		Assert.assertEquals(1, canonicalGrammar.getNonterminalDefinitions().get("p").getAlternatives().get(1).getExpansion().getElements().size());
+		Assert.assertEquals("p", canonicalGrammar.getNonterminalDefinitions().get("p").getAlternatives().get(1).getExpansion().getElements().get(0).getSymbol());
 
 		Assert.assertEquals("q", canonicalGrammar.getNonterminalDefinitions().get("q").getName());
 		Assert.assertEquals(1, canonicalGrammar.getNonterminalDefinitions().get("q").getAlternatives().size());
-		Assert.assertNull(canonicalGrammar.getNonterminalDefinitions().get("q").getAlternatives().get(0).getEffectivePrecedenceTerminal());
-		Assert.assertEquals(2, canonicalGrammar.getNonterminalDefinitions().get("q").getAlternatives().get(0).getExpansion().size());
-		Assert.assertEquals("f", canonicalGrammar.getNonterminalDefinitions().get("q").getAlternatives().get(0).getExpansion().get(0));
-		Assert.assertEquals("g", canonicalGrammar.getNonterminalDefinitions().get("q").getAlternatives().get(0).getExpansion().get(1));
+		Assert.assertNull(getEffectivePrecedenceTerminal(canonicalGrammar.getNonterminalDefinitions().get("q").getAlternatives().get(0)));
+		Assert.assertEquals(2, canonicalGrammar.getNonterminalDefinitions().get("q").getAlternatives().get(0).getExpansion().getElements().size());
+		Assert.assertEquals("f", canonicalGrammar.getNonterminalDefinitions().get("q").getAlternatives().get(0).getExpansion().getElements().get(0).getSymbol());
+		Assert.assertEquals("g", canonicalGrammar.getNonterminalDefinitions().get("q").getAlternatives().get(0).getExpansion().getElements().get(1).getSymbol());
 
 		Assert.assertEquals("start", canonicalGrammar.getNonterminalDefinitions().get("start").getName());
 		Assert.assertEquals(1, canonicalGrammar.getNonterminalDefinitions().get("start").getAlternatives().size());
-		Assert.assertNull(canonicalGrammar.getNonterminalDefinitions().get("start").getAlternatives().get(0).getEffectivePrecedenceTerminal());
-		Assert.assertEquals(1, canonicalGrammar.getNonterminalDefinitions().get("start").getAlternatives().get(0).getExpansion().size());
-		Assert.assertEquals("e", canonicalGrammar.getNonterminalDefinitions().get("start").getAlternatives().get(0).getExpansion().get(0));
+		Assert.assertNull(getEffectivePrecedenceTerminal(canonicalGrammar.getNonterminalDefinitions().get("start").getAlternatives().get(0)));
+		Assert.assertEquals(1, canonicalGrammar.getNonterminalDefinitions().get("start").getAlternatives().get(0).getExpansion().getElements().size());
+		Assert.assertEquals("e", canonicalGrammar.getNonterminalDefinitions().get("start").getAlternatives().get(0).getExpansion().getElements().get(0).getSymbol());
 
 		//
 		// other
@@ -131,6 +132,15 @@ public class GrammarCanonicalizerTest {
 
 		Assert.assertEquals(START_NONTERMINAL_NAME, canonicalGrammar.getStartNonterminalName());
 
+	}
+
+	private static String getEffectivePrecedenceTerminal(name.martingeisse.mapag.grammar.canonical.Alternative alternative) {
+		AlternativeConflictResolver resolver = alternative.getConflictResolver();
+		if (resolver == null) {
+			return null;
+		}
+		Assert.assertNull(resolver.getTerminalToResolution());
+		return resolver.getEffectivePrecedenceTerminal();
 	}
 
 }
