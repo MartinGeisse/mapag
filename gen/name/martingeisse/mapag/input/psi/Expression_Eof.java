32816ebc14745b4742bf351eb981fc7f881e60fc
Martin Geisse <martingeisse@googlemail.com>
2017-11-26 20:44:58 +0100

    allow %eof on the right-hand side of productions

---
+++ gen/name/martingeisse/mapag/input/psi/Expression_Eof.java
@@ -0,0 +1,12 @@
+package name.martingeisse.mapag.input.psi;
+
+import com.intellij.lang.ASTNode;
+import org.jetbrains.annotations.NotNull;
+
+public final class Expression_Eof extends Expression {
+
+	public Expression_Eof(@NotNull ASTNode node) {
+		super(node);
+	}
+
+}
