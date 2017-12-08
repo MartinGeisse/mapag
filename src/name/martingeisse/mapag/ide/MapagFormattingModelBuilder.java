857e9a6e19eabe059ce431712474267efb71793a
Martin Geisse <martingeisse@googlemail.com>
2017-12-08 12:00:40 +0100

    formatting, WIP

--- src/name/martingeisse/mapag/ide/MapagFormattingModelBuilder.java
+++ src/name/martingeisse/mapag/ide/MapagFormattingModelBuilder.java
@@ -9,7 +9,9 @@ import com.intellij.psi.TokenType;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.formatter.common.AbstractBlock;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
 import com.intellij.util.containers.ContainerUtil;
+import name.martingeisse.mapag.input.Symbols;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -20,13 +22,73 @@ import java.util.List;
  */
 public class MapagFormattingModelBuilder implements FormattingModelBuilder {
 
+	private static final TokenSet DEFAULT_INDENTED_SYMBOLS = TokenSet.create(
+
+		// list elements for which the list is already indented normally
+		Symbols.terminalDeclaration,
+		Symbols.precedenceDeclaration_Normal,
+		Symbols.precedenceDeclaration_ErrorWithSemicolon,
+		Symbols.precedenceDeclaration_ErrorWithoutSemicolon,
+		Symbols.resolveDeclaration,
+
+		// terminal list inside a precedence declaration
+		Symbols.precedenceDeclaration_Normal_Terminals_List,
+		Symbols.precedenceDeclarationSymbol
+
+	);
+
+	private static final TokenSet NORMALLY_INDENTED_SYMBOLS = TokenSet.create(
+		Symbols.grammar_TerminalDeclarations_Identifiers_List,
+		Symbols.grammar_PrecedenceTable_PrecedenceDeclarations_List,
+		Symbols.production_Multi_Alternatives_List,
+		Symbols.alternativeAttribute_ResolveBlock_ResolveDeclarations_List
+	);
+
+	private static final TokenSet NOT_INDENTED_SYMBOLS = TokenSet.create(
+
+		// relative to its parent, a closing brace is never indented
+		Symbols.CLOSING_CURLY_BRACE,
+
+		// comments are not indented relative to their parent, which is usually indented itself
+		Symbols.LINE_COMMENT,
+		Symbols.BLOCK_COMMENT,
+
+		// top-level elements
+		Symbols.grammar_TerminalDeclarations,
+		Symbols.grammar_PrecedenceTable,
+		Symbols.grammar_Productions_List,
+		Symbols.production_SingleUnnamed,
+		Symbols.production_SingleNamed,
+		Symbols.production_Multi,
+		Symbols.production_ErrorWithNonterminalNameWithSemicolon,
+		Symbols.production_ErrorWithNonterminalNameWithClosingCurlyBrace,
+		Symbols.production_ErrorWithNonterminalNameAtEof,
+		Symbols.production_ErrorWithoutNonterminalNameWithSemicolon,
+		Symbols.production_ErrorWithoutNonterminalNameWithClosingCurlyBrace,
+		Symbols.production_ErrorWithoutNonterminalNameAtEof
+
+	);
+
 	@NotNull
 	@Override
 	public FormattingModel createModel(PsiElement element, CodeStyleSettings settings) {
-
-
 		MyBlock block = new MyBlock(element.getNode(), null);
+
+		// TODO remove ------
+		dump(block, 0);
+		// TODO remove ------
+
 		return FormattingModelProvider.createFormattingModelForPsiFile(element.getContainingFile(), block, settings);
+	}
+
+	private void dump(MyBlock block, int dumpIndent) {
+		for (int i=0; i<dumpIndent; i++) {
+			System.out.print("  ");
+		}
+		System.out.println(block.getNode().getElementType() + " -- indent: " + block.getIndent());
+		for (Block child : block.getSubBlocks()) {
+			dump((MyBlock)child, dumpIndent + 1);
+		}
 	}
 
 	@Nullable
@@ -35,12 +97,6 @@ public class MapagFormattingModelBuilder implements FormattingModelBuilder {
 		return null;
 	}
 
-	// TODO null indent means default indent -- check if this is sufficient
-	// TODO wrap is "always", "never" or "if line is too long" -- soudns logical
-	// TODO spacing says min/max spaces and whether line break. Can use SpacingBuilder.
-	// Note the difference: line break == this belongs into the next line; wrap == line is too long
-	// TODO alignment: align symbols in subsequent lines, e.g. colons in a JSON object. I hate that anyway.
-	// TODO getChildAttributes(): Supports indentation at the moment the user breaks a line manually
 	public static class MyBlock extends AbstractBlock {
 
 		public MyBlock(@NotNull ASTNode node, @Nullable Wrap wrap) {
@@ -59,7 +115,6 @@ public class MapagFormattingModelBuilder implements FormattingModelBuilder {
 		}
 
 		private Block buildChild(ASTNode childNode) {
-			// TODO choose indent, spacing, wrap
 			return new MyBlock(childNode, null);
 		}
 
@@ -67,55 +122,25 @@ public class MapagFormattingModelBuilder implements FormattingModelBuilder {
 		public Indent getIndent() {
 
 			// TODO re-write when PSI is stable
+			// TODO null indent means default indent -- check if this is sufficient
+			// TODO wrap is "always", "never" or "if line is too long" -- soudns logical
+			// TODO spacing says min/max spaces and whether line break. Can use SpacingBuilder.
+			// Note the difference: line break == this belongs into the next line; wrap == line is too long
+			// TODO alignment: align symbols in subsequent lines, e.g. colons in a JSON object. I hate that anyway.
+			// TODO getChildAttributes(): Supports indentation at the moment the user breaks a line manually
 
-//			IElementType type = myNode.getElementType();
-//			IElementType parentType = (myNode.getTreeParent() == null ? null : myNode.getTreeParent().getElementType());
-//
-//			// relative to its parent, a closing brace is never indented
-//			if (type == Symbols.CLOSING_CURLY_BRACE) {
-//				return Indent.getNoneIndent();
-//			}
-//
-//			// indent terminals inside their curly-braces block by one level, but no further indentation of terminals
-//			// inside the terminal list (specifcally, no continuation indent -- the braces already delimit that list).
-//			if (type == Symbols.terminalDeclarations) {
-//				return Indent.getNormalIndent();
-//			}
-//			if (isInside(myNode, Symbols.terminalDeclaration)) {
-//				return Indent.getNoneIndent();
-//			}
-//
-//			// Indent the precedence table inside its braces. Unlike the terminals, this table is separated into lines,
-//			// so continuation indent is okay.
-//			if (type == Symbols.grammar_PrecedenceTable) {
-//				return Indent.getNormalIndent();
-//			}
-//			if (isInside(myNode, Symbols.grammar_PrecedenceTable)) {
-//				return null;
-//			}
-//
-//			// Indent the resolve declarations inside a resolve block, but not their list nodes
-//			if (type == Symbols.resolveDeclaration) {
-//				return Indent.getNormalIndent();
-//			}
-//			if (type == Symbols.alternativeAttribute_ResolveBlock_ResolveDeclarations_Start || type == Symbols.alternativeAttribute_ResolveBlock_ResolveDeclarations_Next) {
-//				return Indent.getNoneIndent();
-//			}
-//
-//			// don't indent any top-level elements (including productions and production list nodes)
-//			if (parentType == Symbols.grammar || parentType == Symbols.grammar_Productions_Start || parentType == Symbols.grammar_Productions_Next) {
-//				return Indent.getNoneIndent();
-//			}
-//
-//			// indent the alternatives within a multi-alternative production, but not their list nodes
-//			if (type == Symbols.production_Multi_Alternatives_1_Named || type == Symbols.production_Multi_Alternatives_1_Unnamed) {
-//				return Indent.getNormalIndent();
-//			}
-//			if (type == Symbols.production_Multi_Alternatives_Start || type == Symbols.production_Multi_Alternatives_Next) {
-//				return Indent.getNoneIndent();
-//			}
-
+			IElementType type = myNode.getElementType();
+			if (DEFAULT_INDENTED_SYMBOLS.contains(type)) {
+				return null;
+			}
+			if (NORMALLY_INDENTED_SYMBOLS.contains(type)) {
+				return Indent.getNormalIndent();
+			}
+			if (NOT_INDENTED_SYMBOLS.contains(type)) {
+				return Indent.getNoneIndent();
+			}
 			return null;
+
 		}
 
 		private boolean isInside(ASTNode node, IElementType type) {
