d9fb1aaad09feeec6fd3331b6eb4f3c0fa610e4c
Martin Geisse <martin.geisse@servicereisen.de>
2018-04-09 14:13:26 +0200

    standalone parser generation, WIP

--- src/name/martingeisse/mapag/bootstrap/MapagGrammarParserGenerationMain.java
+++ src/name/martingeisse/mapag/bootstrap/MapagGrammarParserGenerationMain.java
@@ -1,11 +1,7 @@
 package name.martingeisse.mapag.bootstrap;
 
 import com.google.common.collect.ImmutableList;
-import name.martingeisse.mapag.codegen.CodeGenerationParameters;
-import name.martingeisse.mapag.codegen.Configuration;
-import name.martingeisse.mapag.codegen.OutputFileFactory;
-import name.martingeisse.mapag.codegen.CodeGenerationDriver;
-import name.martingeisse.mapag.codegen.StandaloneCodeGenerationContext;
+import name.martingeisse.mapag.codegen.*;
 import name.martingeisse.mapag.grammar.canonical.info.GrammarInfo;
 import name.martingeisse.mapag.grammar.canonicalization.GrammarCanonicalizer;
 import name.martingeisse.mapag.grammar.extended.Grammar;
@@ -336,7 +332,8 @@ public class MapagGrammarParserGenerationMain extends BootstrapBase {
 			}
 
 		};
-		CodeGenerationParameters parameters = new CodeGenerationParameters(grammarInfo, stateMachine, configuration, outputFileFactory, new StandaloneCodeGenerationContext());
+		// CodeGenerationParameters parameters = new CodeGenerationParameters(grammarInfo, stateMachine, configuration, outputFileFactory, new StandaloneCodeGenerationContext());
+		CodeGenerationParameters parameters = new CodeGenerationParameters(grammarInfo, stateMachine, configuration, outputFileFactory, new IntellijCodeGenerationContext());
 		new CodeGenerationDriver(parameters).generate();
 
 	}
