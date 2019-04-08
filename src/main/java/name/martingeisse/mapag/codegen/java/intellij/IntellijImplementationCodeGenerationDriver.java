8442a55ece1acb85431c4ef857924853cf2eb02e
Martin Geisse <martin.geisse@servicereisen.de>
2019-04-08 08:38:03 +0200

    IntelliJ based CM implementation generation, WIP

--- src/main/java/name/martingeisse/mapag/codegen/java/intellij/IntellijImplementationCodeGenerationDriver.java
+++ src/main/java/name/martingeisse/mapag/codegen/java/intellij/IntellijImplementationCodeGenerationDriver.java
@@ -7,7 +7,7 @@ package name.martingeisse.mapag.codegen.java.intellij;
 import name.martingeisse.mapag.codegen.CodeGenerationDriver;
 import name.martingeisse.mapag.codegen.CodeGenerationParameters;
 import name.martingeisse.mapag.codegen.ConfigurationException;
-import org.apache.commons.lang3.NotImplementedException;
+import name.martingeisse.mapag.codegen.java.HelperClassGenerator;
 
 import java.io.IOException;
 
@@ -18,8 +18,17 @@ public class IntellijImplementationCodeGenerationDriver implements CodeGeneratio
 
 	@Override
 	public void generate(CodeGenerationParameters parameters) throws ConfigurationException, IOException {
+		String templateSubfolder = "intellij";
+		String subpackage = "cm.impl";
+
+		// generate helper classes
+		HelperClassGenerator.generate(templateSubfolder, subpackage, "CmListImpl", parameters);
+		HelperClassGenerator.generate(templateSubfolder, subpackage, "CmOptionalImpl", parameters);
+		HelperClassGenerator.generate(templateSubfolder, subpackage, "CmTokenImpl", parameters);
+
 		// TODO
-		throw new NotImplementedException("");
+		// throw new NotImplementedException("");
+
 	}
 
 }
