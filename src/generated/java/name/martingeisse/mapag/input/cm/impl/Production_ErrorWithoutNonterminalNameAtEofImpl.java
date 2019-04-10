2569d2e09205e9abaecf681457a9701f9d1958e4
Martin Geisse <martin.geisse@servicereisen.de>
2019-04-10 08:27:22 +0200

    regenerate code with the new code generator; refactor processing to use the CM

--- src/generated/java/name/martingeisse/mapag/input/cm/impl/Production_ErrorWithoutNonterminalNameAtEofImpl.java
+++ src/generated/java/name/martingeisse/mapag/input/cm/impl/Production_ErrorWithoutNonterminalNameAtEofImpl.java
@@ -30,7 +30,30 @@ public final class Production_ErrorWithoutNonterminalNameAtEofImpl extends Produ
 
     
         
+        
+            public LeafPsiElement getNameIdentifier() {
+                return name.martingeisse.mapag.input.psi.PsiUtil.getNameIdentifier(this);
+            }
+
+            public String getName() {
+                LeafPsiElement nameIdentifier = getNameIdentifier();
+                return (nameIdentifier == null ? null : nameIdentifier.getText());
+            }
+
+            public PsiElement setName(String newName) throws IncorrectOperationException {
+                LeafPsiElement nameIdentifier = getNameIdentifier();
+                if (nameIdentifier == null) {
+                    throw new IncorrectOperationException("name identifier not found");
+                }
+                return (LeafPsiElement) nameIdentifier.replaceWithText(newName);
+            }
+
+        
     
     
+    
+            public void delete() throws IncorrectOperationException {
+            name.martingeisse.mapag.input.psi.PsiUtil.delete(this);
+        }
     
 }
--- src/generated/java/name/martingeisse/mapag/input/cm/impl/Production_MultiImpl.java
+++ src/generated/java/name/martingeisse/mapag/input/cm/impl/Production_MultiImpl.java
@@ -44,7 +44,30 @@ public final class Production_MultiImpl extends ProductionImpl implements Produc
         }
     
         
+        
+            public LeafPsiElement getNameIdentifier() {
+                return name.martingeisse.mapag.input.psi.PsiUtil.getNameIdentifier(this);
+            }
+
+            public String getName() {
+                LeafPsiElement nameIdentifier = getNameIdentifier();
+                return (nameIdentifier == null ? null : nameIdentifier.getText());
+            }
+
+            public PsiElement setName(String newName) throws IncorrectOperationException {
+                LeafPsiElement nameIdentifier = getNameIdentifier();
+                if (nameIdentifier == null) {
+                    throw new IncorrectOperationException("name identifier not found");
+                }
+                return (LeafPsiElement) nameIdentifier.replaceWithText(newName);
+            }
+
+        
     
     
+    
+            public void delete() throws IncorrectOperationException {
+            name.martingeisse.mapag.input.psi.PsiUtil.delete(this);
+        }
     
 }
