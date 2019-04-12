46ff2829943520b487da5451ef735ad1de8f7a0d
Martin Geisse <martin.geisse@servicereisen.de>
2019-04-12 08:16:16 +0200

    standalone code generator, WIP

--- src/main/java/name/martingeisse/mapag/codegen/java/standalone/StandaloneImplementationCodeGenerationDriver.java
+++ src/main/java/name/martingeisse/mapag/codegen/java/standalone/StandaloneImplementationCodeGenerationDriver.java
@@ -6,8 +6,9 @@ package name.martingeisse.mapag.codegen.java.standalone;
 
 import name.martingeisse.mapag.codegen.CodeGenerationDriver;
 import name.martingeisse.mapag.codegen.CodeGenerationParameters;
+import name.martingeisse.mapag.codegen.Configuration;
 import name.martingeisse.mapag.codegen.ConfigurationException;
-import org.apache.commons.lang3.NotImplementedException;
+import name.martingeisse.mapag.codegen.java.HelperClassGenerator;
 
 import java.io.IOException;
 
@@ -18,8 +19,17 @@ public class StandaloneImplementationCodeGenerationDriver implements CodeGenerat
 
 	@Override
 	public void generate(CodeGenerationParameters parameters) throws ConfigurationException, IOException {
+		Configuration configuration = parameters.getConfiguration();
+		String templateSubfolder = "standalone";
+		String subpackage = "cm.impl";
+
+		// generate helper classes
+		HelperClassGenerator.generate(templateSubfolder, subpackage, "CmListImpl", parameters);
+		HelperClassGenerator.generate(templateSubfolder, subpackage, "CmOptionalImpl", parameters);
+		HelperClassGenerator.generate(templateSubfolder, subpackage, "CmTokenImpl", parameters);
+		HelperClassGenerator.generate(templateSubfolder, subpackage, "IElementType", parameters);
+
 		// TODO
-		throw new NotImplementedException("");
 	}
 
 }
